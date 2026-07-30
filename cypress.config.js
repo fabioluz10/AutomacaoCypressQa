3ac276bffc2f2f4da89b7acb362dbd1442cf9d81
Fabio Luz <fabio.luz10@yahoo.com.br>
2026-07-30 17:11:15 -0300

    Alteração de arquivo README

--- cypress.config.js
+++ cypress.config.js
@@ -12,6 +12,7 @@ module.exports = defineConfig({
   screenshotOnRunFailure: true,
   screenshotsFolder: 'cypress/screenshots',
   videosFolder: 'cypress/videos',
+  
   e2e: {
     baseUrl: "https://www.automationpratice.com.br/",
     defaultCommandTimeout: 5000,
