781f9521e884d3e0890a252818eb6ca9ff452e8d
Fabio Luz <fabio.luz10@yahoo.com.br>
2026-07-29 22:37:56 -0300

    Teste negativo com Report2

--- cypress/support/pages/cadastro_usuario_page.js
+++ cypress/support/pages/cadastro_usuario_page.js
@@ -27,7 +27,7 @@ export default {
             .type(email)
     },
     preencherSenha(senha) {
-        cy.get('#password')
+        cy.get('#passwordS')
             .type(senha)
     },
     validarMensagemSucesso(nome) {
